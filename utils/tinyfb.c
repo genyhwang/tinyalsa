70c274dcee65cb52817ca8715635188efe4e7122
genyhwang <genyhwang@users.noreply.github.com>
2024-07-02 00:44:21 +0800

    Update tinyfb.c

--- utils/tinyfb.c
+++ utils/tinyfb.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <fcntl.h>
 
 int main(int argc, const char **argv)
 {
+	printf("argc: %d, argv: %s\n", argc, argv[0]);
 	int fd = 0;
 	fd = open("/dev/fb0", O_RDWR);
 	if (fd < 0) {
